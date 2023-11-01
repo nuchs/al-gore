f2f0fbf5ab8404679ff98e703e261b3ad7ab7f40
nuchs <sjbrown@live.co.uk>
2023-11-01 21:25:32 +0000

    Switch to using errors

--- test_runner.go
+++ test_runner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -14,35 +15,35 @@ func runTests(tests []test, baseData []testData) {
 			asc := benchmark(t.sorter, set.asc)
 			desc := benchmark(t.sorter, set.desc)
 			rand := benchmark(t.sorter, set.rand)
-			fmt.Printf("%s\t%8d µs  %8d µs  %8d µs\n", set.size, asc, desc, rand)
+			fmt.Printf("%s\t%8v  %8v  %8v\n", set.size, asc, desc, rand)
 		}
 		fmt.Println()
 	}
 }
 
-func benchmark(test sorter, data []int) int {
+func benchmark(test sorter, data []int) string {
 	const iterations = 1
 	total := 0
 	for i := 0; i < iterations; i++ {
-		result := runTest(test, data)
-		if result == -1 {
-			return -1
+		if result, err := runTest(test, data); err != nil {
+			return err.Error()
+		} else {
+			total += result
 		}
-		total += result
 	}
 
-	return total / iterations
+	return fmt.Sprintf("%d µs", total/iterations)
 }
 
-func runTest(test sorter, baseData []int) int {
+func runTest(test sorter, baseData []int) (int, error) {
 	data := copyData(baseData)
 	start := time.Now()
 	test(data)
 	elapsed := time.Since(start)
 
 	if !isSorted(data) {
-		return -1
+		return -1, errors.New("Fail")
 	} else {
-		return int(elapsed.Microseconds())
+		return int(elapsed.Microseconds()), nil
 	}
 }
